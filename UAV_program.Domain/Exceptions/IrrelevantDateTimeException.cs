87e9b80372f528d83896d18471de642e90dda908
Max Kornilev <kornilev.maxim@mail.ru>
2025-01-06 15:59:00 +0300

    fixed naming

---
+++ UAV_program.Domain/Exceptions/IrrelevantDateTimeException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UAV_program.Domain.Exceptions
+{
+	public class IrrelevantDateTimeException : Exception
+	{
+		public IrrelevantDateTimeException() { }
+		public IrrelevantDateTimeException(string message) : base(message) { }
+		public IrrelevantDateTimeException(string message, Exception inner) : base(message, inner) { }
+	}
+}
